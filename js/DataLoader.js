f4c9af8fcc2f0c9af2728b369e480750b9935b9c
Neranjan17 <nsbwtennekoon@gmail.com>
2025-12-18 08:34:05 +0530

    update

--- js/DataLoader.js
+++ js/DataLoader.js
@@ -3,7 +3,7 @@ class DataLoader {
     this.members = ['MID01-G50-001-01', 'MID00-G50-000-01', 'MID01-G50-002-01', 'MID00-G50-000-02'];
 
     this.familyTree = {
-      profilePic: '../assets/members-img/person.png',
+      profilePic: './assets/members-img/person.png',
       mainTitle: 'John Doe',
       subTitle: '1950 - 2015',
       description:
@@ -21,7 +21,7 @@ class DataLoader {
 
   getMemberDataById(memberId) {
 
-    const memberImgLink = '../assets/members-img/person.png';
+    const memberImgLink = './assets/members-img/person.png';
     const memberShortName = 'Susila Tennekoon';
     const memberFullName = 'Don Basil wijayawarhana tennekoon';
     const memberLineageName = 'Member of Wijayawarhana Tennekoon lineage';
@@ -48,30 +48,30 @@ class DataLoader {
     ];
 
     const wivesDataList = [
-        { wifeImg: "../assets/members-img/person.png", marriageFate: "Married wife", wifeName: "Dona Martha Kumari", marriageInfo: "Married In 1962 in Kotte, Colombo.", wifeId: "MID01-G51-001-01" }
+        { wifeImg: "./assets/members-img/person.png", marriageFate: "Married wife", wifeName: "Dona Martha Kumari", marriageInfo: "Married In 1962 in Kotte, Colombo.", wifeId: "MID01-G51-001-01" }
     ];
 
     const childrenDataList = [
-        { childImg: "../assets/members-img/person.png", childName: "Don Frisians silva", childInfo: "1st son of Lalith kumara and Mala.", childId: "MID01-G51-002-01" },
-        { childImg: "../assets/members-img/person.png", childName: "Don Frisians silva", childInfo: "2st son of Lalith kumara and Mala.", childId: "MID00-G51-000-03" },
-        { childImg: "../assets/members-img/person.png", childName: "Don Frisians silva", childInfo: "Son of Lalith kumara and Mala.", childId: "MID00-G51-000-04" }
+        { childImg: "./assets/members-img/person.png", childName: "Don Frisians silva", childInfo: "1st son of Lalith kumara and Mala.", childId: "MID01-G51-002-01" },
+        { childImg: "./assets/members-img/person.png", childName: "Don Frisians silva", childInfo: "2st son of Lalith kumara and Mala.", childId: "MID00-G51-000-03" },
+        { childImg: "./assets/members-img/person.png", childName: "Don Frisians silva", childInfo: "Son of Lalith kumara and Mala.", childId: "MID00-G51-000-04" }
     ];
 
     const parentDataList = [
-        { parentImg: "../assets/members-img/person.png", parentRelation: "Mother", parentName: "Don Frisians silva", parentId: "MID01-G52-001-01" },
-        { parentImg: "../assets/members-img/person.png", parentRelation: "Father", parentName: "Don Frisians silva", parentId: "MID00-G52-000-05" }
+        { parentImg: "./assets/members-img/person.png", parentRelation: "Mother", parentName: "Don Frisians silva", parentId: "MID01-G52-001-01" },
+        { parentImg: "./assets/members-img/person.png", parentRelation: "Father", parentName: "Don Frisians silva", parentId: "MID00-G52-000-05" }
     ];
 
     const siblingsDataList = [
-        { siblingImg: "../assets/members-img/person.png", siblingRelation: "Elder brother", siblingName: "Don Frisians silva", siblingId: "MID01-G52-001-02" },
-        { siblingImg: "../assets/members-img/person.png", siblingRelation: "Younger brother", siblingName: "Don Frisians silva", siblingId: "MID00-G52-000-06" },
-        { siblingImg: "../assets/members-img/person.png", siblingRelation: "Younger Sister", siblingName: "Don Frisians silva", siblingId: "MID01-G52-001-03" }
+        { siblingImg: "./assets/members-img/person.png", siblingRelation: "Elder brother", siblingName: "Don Frisians silva", siblingId: "MID01-G52-001-02" },
+        { siblingImg: "./assets/members-img/person.png", siblingRelation: "Younger brother", siblingName: "Don Frisians silva", siblingId: "MID00-G52-000-06" },
+        { siblingImg: "./assets/members-img/person.png", siblingRelation: "Younger Sister", siblingName: "Don Frisians silva", siblingId: "MID01-G52-001-03" }
     ];
 
     const sourcesDataList = [
-        { sourceIcon: "../images/sourece-doc-icon.svg", sourceName: "Family Chronicle", sourceNote: "Received training in royal military arts", sourcePath: "../assets/members-sources/dommy-source.png" },
-        { sourceIcon: "../images/sourece-img-icon.svg", sourceName: "Family Chronicle", sourceNote: "Received training in royal military arts", sourcePath: "../assets/members-sources/dommy-source.png" },
-        { sourceIcon: "../images/sourece-img-icon.svg", sourceName: "Family Chronicle", sourceNote: "Received training in royal military arts", sourcePath: "../assets/members-sources/dommy-source.png" }
+        { sourceIcon: "../images/sourece-doc-icon.svg", sourceName: "Family Chronicle", sourceNote: "Received training in royal military arts", sourcePath: "./assets/members-sources/dommy-source.png" },
+        { sourceIcon: "../images/sourece-img-icon.svg", sourceName: "Family Chronicle", sourceNote: "Received training in royal military arts", sourcePath: "./assets/members-sources/dommy-source.png" },
+        { sourceIcon: "../images/sourece-img-icon.svg", sourceName: "Family Chronicle", sourceNote: "Received training in royal military arts", sourcePath: "./assets/members-sources/dommy-source.png" }
     ];
 
     return {
